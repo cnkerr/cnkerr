110a54c3086e59290d20616ec049cb8fa24bd4e8
cnkerr <ckjenkem@gmail.com>
2026-01-03 10:21:31 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,75 @@
+window.DATA_REMOTE = {
+slides: [
+  { src: "market.jpg",  caption: 'Snyders, "Market Scene"' },
+  { src: "flowers.jpg", caption: 'Bruegel the Younger, "Basket of Flowers"' },
+  { src: "conjurer.jpg",caption: 'H. Bosch, "The Conjurer"' },
+  { src: "icarus.jpg",  caption: 'Bruegel the Elder, "Landscape w/ the Fall..."' }
+],
+
+writing: [
+      { title:"Taryn Simon Mines the Empty Aesthetics of Diplomacy", outlet:"Hyperallergic", year:2016, url:"http://hyperallergic.com/282066/taryn-simon-mines-the-empty-aesthetics-of-diplomacy/" },
+      { title:"Rolling Through Barbara Kruger's Slick Skatepark Installation", outlet:"Hyperallergic", year:2017, url:"https://hyperallergic.com/410095/barbara-kruger-les-coleman-skatepark-performa/" },
+      { title:"The Power of Protest Art and the Dignity of the Protester", outlet:"Hyperallergic", year:2017, url:"https://hyperallergic.com/the-power-of-protest-art-and-the-dignity-of-the-protester/" },
+      { title:"A Collaborative Reading of 'Song of Myself'", outlet:"Hyperallergic", year:2017, url:"https://hyperallergic.com/373782/a-collaborative-reading-of-whitmans-song-of-myself-all-across-alabama/" },
+      { title:"Imagining a Sustainable Skateboard", outlet:"Village Psychic", year:2022, url:"https://www.villagepsychic.net/blog/imagining-a-sustainable-skateboard?rq=kerr" },
+      { title:"Climate Change: the Ultimate Skatestopper", outlet:"Village Psychic", year:2020, url:"https://www.villagepsychic.net/blog//climate-change-the-ultimate-skatestopper" },
+      { title:"Considering the Ideal Skate Outfit", outlet:"Village Psychic", year:2020, url:"https://www.villagepsychic.net/blog/what-is-the-ultimate-skate-silhouette" },
+      { title:"The Activists of Crimson Tide Country", outlet:"The New Yorker", year:2016, url:"http://www.newyorker.com/news/sporting-scene/the-activists-of-crimson-tide-country" },
+      { title:"Best of 2021: Trung, 'RESPECTFULLY'", outlet:"Quartersnacks", year:2021, url:"https://quartersnacks.com/2021/12/the-best-skate-videos-parts-of-2021-qs-readers-poll-results/" },
+      { title:"Best of 2022: Tyshawn, 'The General'", outlet:"Quartersnacks", year:2022, url:"https://quartersnacks.com/2022/12/the-best-skate-videos-parts-of-2022-qs-readers-poll-results/" },
+      { title:"Best of 2023: Shanahan, 'Double-Up'", outlet:"Quartersnacks", year:2023, url:"https://quartersnacks.com/2023/12/the-best-skate-videos-parts-of-2023-qs-readers-poll-results/" },
+      { title:"Best of 2024: Deedz, 'Uneven'", outlet:"Quartersnacks", year:2024, url:"https://quartersnacks.com/2024/12/the-best-skate-videos-parts-of-2024-qs-readers-poll-results/" },
+      { title:"Best of 2025: Gustav, '3:2'", outlet:"Quartersnacks", year:2025, url:"https://quartersnacks.com/2025/12/the-best-skate-videos-parts-of-2025-qs-readers-poll-results/" },
+      { title:"Waxing Poetic #1: 'Prufrock'", outlet:"Simple Magic", year:2025, url:"https://www.simplemagic.ca/waxing-poetic-1-the-love-song-of-j-alfred-prufrock/" },
+      { title:"Waxing Poetic #2: 'Slam, Dunk & Hook'", outlet:"Simple Magic", year:2025, url:"https://www.simplemagic.ca/waxing-poetic-2-slam-dunk-hook/" },
+      { title:"Waxing Poetic #3: 'The Second Coming'", outlet:"Simple Magic", year:2025, url:"https://www.simplemagic.ca/waxing-poetic-3-the-second-coming/" },
+      { title:"Mark Ronson&Lucky Daye for Audemars Piguet", outlet:"Complex", year:2022, url:"https://www.complex.com/style/a/christiannkerr/mark-ronson-lucky-daye-new-song-too-much-audemars-piguet" },
+      { title:"China's Li-Ning Wants Erik Ellington's Help", outlet:"Complex", year:2021, url:"https://www.complex.com/sneakers/a/christiankerr/li-ning-erik-ellington-signature-model-skateboard-collection" },
+      { title:"How Ebonie Ward Manages Future", outlet:"Complex", year:2020, url:"https://www.complex.com/music/a/christiannkerr/how-ebonie-ward-built-her-team" },
+      { title:"Can Fashion Subvert the Surveillance State?", outlet:"Secret Mag", year:2021, url:"https://secretmag.net/2021/11/10/surveillance-fashion-metaverse/" },
+      { title:"A Chat w/ Andrew Considine, WKND Pro", outlet:"Free Skate Mag", year:2022, url:"https://www.freeskatemag.com/2022/01/19/wknds-street-fighters-ii-andrew-considine-interview/" },
+      { title:"Meet the YouTube Stars Disrupting Skateboarding", outlet:"Jenkem Mag", year:2015, url:"https://www.jenkemmag.com/home/2015/04/24/meet-the-youtube-stars-that-are-disrupting-skateboarding/" },
+      { title:"Remembering David Bowie Through Skate Videos", outlet:"Jenkem Mag", year:2016, url:"https://www.jenkemmag.com/home/2016/01/11/remembering-david-bowie-through-skate-videos/" },
+      { title:"Talking to a Professional Fingerboarder", outlet:"Jenkem Mag", year:2015, url:"https://www.jenkemmag.com/home/2015/12/14/an-interview-with-a-professional-fingerboarder/" },
+      { title:"Finding the Guy Who Tried the 13-Flat-13 in the Rain", outlet:"Jenkem Mag", year:2015, url:"https://www.jenkemmag.com/home/2015/09/02/an-interview-with-the-guy-who-tried-the-13-flat-13-in-the-rain/" },
+      { title:"4 Skateboarding Events the Olympics Can Have", outlet:"Jenkem Mag", year:2015, url:"https://www.jenkemmag.com/home/2015/08/04/4-skateboarding-events-the-olympics-can-have/" },
+      { title:"Meet Neftalie, US Ambassador of Skateboarding", outlet:"Jenkem Mag", year:2017, url:"https://www.jenkemmag.com/home/2017/03/01/americas-ambassador-of-skateboarding-neftalie-williams/" },
+      { title:"The Skateboarder's Guide to the 2016 Presidential Election", outlet:"Jenkem Mag", year:2016, url:"https://www.jenkemmag.com/home/2016/04/18/the-skateboarders-guide-to-the-2016-u-s-presidential-election/" },
+      { title:"The Death & Life of the Great American Skate Plaza", outlet:"Jenkem Mag", year:2016, url:"https://www.jenkemmag.com/home/2016/03/11/the-death-and-life-of-great-american-skate-plazas/" },
+      { title:"7 Tricks to Make You Look Better at Skating Than You Really Are", outlet:"Jenkem Mag", year:2020, url:"https://www.jenkemmag.com/home/2020/09/10/7-tricks-make-look-better-at-skating-than-you-really-are/" },
+      { title:"A Skater's Guide to Attending a Protest", outlet:"Jenkem Mag", year:2020, url:"https://www.jenkemmag.com/home/2020/06/04/skaters-guide-attending-protest/" },
+      { title:"Is it Safe to Skate in the Time of Coronavirus?", outlet:"Jenkem Mag", year:2020, url:"https://www.jenkemmag.com/home/2020/03/17/safe-skate-time-coronavirus/" },
+      { title:"Appreciating the Latino Immigrants that Shaped Skateboarding", outlet:"Jenkem Mag", year:2019, url:"https://www.jenkemmag.com/home/2019/08/09/appreciating-latinx-immigrants-shaped-skateboarding/" },
+ { title:"An American in Anshan, China", outlet:"Tumblr", year:2013, url:"https://www.tumblr.com/american-in-anshan-blog/" },
+    ],
+    
+research: [
+      { title:"House of Gates", outlet:"Nicholas Kulish (FSG)", year:2026, url:"https://www.nytimes.com/by/nicholas-kulish", italic:true },
+      { title:"“The Betrayal”", outlet:"George Packer (The Atlantic)", year:2022, url:"https://www.theatlantic.com/magazine/archive/2022/03/biden-afghanistan-exit-american-allies-abandoned/621307/" },
+      { title:"Truly Like Lightning", outlet:"David Duchovny (FSG)", year:2018, url:"https://us.macmillan.com/books/9781250791108/trulylikelightning/", italic:true },
+      { title:"Our Man", outlet:"George Packer (Knopf)", year:2018, url:"https://www.penguinrandomhouse.com/books/215731/our-man-by-george-packer/", italic:true }
+    ],
+    
+factChecking: [
+      { title:"The Magic Mountain Saved My Life", author:"George Packer", publication:"The Atlantic", date:"Dec 2024", url:"https://www.theatlantic.com/magazine/archive/2024/12/thomas-mann-magic-mountain-cultural-political-relevance/680400/" },
+      { title:"No One Gave a Speech Like Patrick Henry", author:"Drew Gilpin Faust", publication:"The Atlantic", date:"Nov 2025", url:"https://www.theatlantic.com/magazine/archive/2025/11/patrick-henry-revolutionary-orator/684308/" },
+      { title:"The Many Lives of Eliza Schuyler", author:"Jane Kamensky", publication:"The Atlantic", date:"Nov 2025", url:"https://www.theatlantic.com/magazine/archive/2025/11/american-revolution-women-eliza-schuyler-abigail-adams/684331/" },
+      { title:"The Judgments of Muriel Spark", author:"Judith Shulevitz", publication:"The Atlantic", date:"Sep 2025", url:"https://www.theatlantic.com/magazine/archive/2025/09/muriel-spark-religion-writing/683550/" },
+      { title:"The Insurrection Problem", author:"Jeffrey Rosen", publication:"The Atlantic", date:"Nov 2025", url:"https://www.theatlantic.com/magazine/archive/2025/11/shays-rebellion-hamilton-jefferson-political-ideologies/684319/" },
+      { title:"What I Learned from Sammy Davis Jr.", author:"Questlove", publication:"The Atlantic", date:"Dec 2025", url:"https://www.theatlantic.com/magazine/2025/12/questlove-sammy-davis-jr-career/684605/" }
+    ],
+
+timeQuotes: [
+      { quote:"...and then I was in time again, hearing the watch. It was Grandfather’s and when Father gave it to me he said, Quentin, I give you the mausoleum of all hope and desire [...] I give it to you not that you may remember time, but that you might forget it now and then for a moment and not spend all your breath trying to conquer it. Because no battle is ever won he said. They are not even fought. The field only reveals to man his own folly and despair, and victory is an illusion of philosophers and fools.", work:"The Sound and the Fury", author:"Faulkner" },
+      { quote:"Yet the timeless in you is aware of life’s timelessness, / And knows that yesterday is but today’s memory and tomorrow is today’s dream. / And that that which sings and contemplates in you is still dwelling within the bounds of that first moment which scattered the stars into space. / Who among you does not feel that his power to love is boundless?", work:"The Prophet", author:"Khalil Gibran" },      
+      { quote:"Thus the whirligig of time brings in his revenges.", work:"Twelfth Night", author:"Shakespeare" },
+	{ quote:"Thus we play the fools with the time, and the spirits of the wise sit in the clouds and mock us.", work:"King Henry IV", author:"Shakespeare" },  
+	{ quote:"The Moving Finger writes; and, having writ, Moves on: nor all thy Piety nor Wit shall lure it back to cancel half a Line, Nor all thy Tears wash out a Word of it.", work:"Rubáiyát", author:"Omar Khayyám" },
+      { quote:"It ain't no time, it ain't no time, it ain't no time. (Ain't no time.)", work:"Aint No Time", author:"Future" },
+	{ quote:"You can be sure / Some go low to get high / You may hurt 'til you cry / You may die (You may die) / Keep on trying (Keep on trying) / 'Til it's summer, in the city / 'Til it's summer, in the city.", work:"You May Die", author:"Outkast" },
+	{ quote:"Modernity is the transient, the fleeting, the contingent; it is one half of art, the other being the-eternal and the immovable.", work:"The Painter of Modern Life", author:"Baudelaire" },
+	{ quote:"For, lo! the winter is past, the rain is over and gone; the flowers appear on the earth; the time of the singing of birds is come, and the voice of the turtle is heard in our land.", work:"The Song of Solomon", author:"the Bible" },      
+	{ quote:"Millions of spiritual creatures walk the earth / Unseen, both when we wake and when we sleep.", work:"Paradise Lost", author:"Milton" },
+	{ quote:"There is a time for some things, and a time for all things; a time for great things, and a time for small things.", work:"Don Quixote", author:"Cervantes" }
+    ]
+  };
